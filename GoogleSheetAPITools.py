85d9c06e41bbd95e3f74fcceb2427e1e291bdec6
dwhodges2 <dwhodges@gmail.com>
2019-04-09 12:12:49 -0400

    add regex col queries

--- GoogleSheetAPITools.py
+++ GoogleSheetAPITools.py
@@ -1,10 +1,10 @@
-from __future__ import print_function
 from googleapiclient import discovery
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 from pprint import pprint
 import json
+import re
 
 
 # If modifying these scopes, delete the file token.json.
@@ -12,11 +12,35 @@ import json
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 
 
-
 def main():
-    
+
+    # Test code here.
+
     quit()
 
+
+def getSheetInfo(sheet):
+    # Return data about a spreadsheet.
+    service = googleAuth()
+    spreadsheet_id = sheet
+    request = service.spreadsheets().get(spreadsheetId=spreadsheet_id, includeGridData=False)
+    response = request.execute()
+    return response
+
+
+def getSheetTabs(sheet):
+    # Return a list of tab names for a given sheet.
+    service = googleAuth()
+    spreadsheet_id = sheet
+    request = service.spreadsheets().get(spreadsheetId=spreadsheet_id, includeGridData=False)
+    sheet_data = request.execute()
+    the_sheets=sheet_data['sheets']
+    the_tabs = []
+    for s in the_sheets:
+        the_title = s['properties']['title']
+        the_tabs.append(the_title)
+
+    return the_tabs
 
 
 def getSheetData(sheet,range):
@@ -29,7 +53,8 @@ def getSheetData(sheet,range):
     # https://developers.google.com/sheets/api/reference/rest/v4/DateTimeRenderOption
     date_time_render_option = 'SERIAL_NUMBER' 
     request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_, valueRenderOption=value_render_option, dateTimeRenderOption=date_time_render_option)
-    response = request.execute()
+    the_data = request.execute()
+    response = the_data["values"]
     return response
 
 
@@ -69,7 +94,8 @@ def sheetAppend(sheet,range,data):
 
 def sheetLookup(sheet,range,search_str,col_search,col_result):
     # Provide sheet, range to search, string to match, the column to match in, and col(s) to return. The col_result can either be an integer or a list of integers, e.g., col_search=0, col_result=[1,2], which will return an array of results. Will return multiple matches in a list.
-    theData = getSheetData(sheet,range).get("values")
+    # theData = getSheetData(sheet,range).get("values")
+    theData = getSheetData(sheet,range)
     theResults = []
     returnCols = []
     if isinstance(col_result,list):
@@ -105,6 +131,76 @@ def batchGetByDataFilter(sheet,datafilters):
 
     # TODO: Change code below to process the `response` dict:
     return response
+
+
+def getMatchingRows(sheet,range,queries, regex=True, operator='or'):
+    # Return a list of rows for which at least one queried column matches regex query. Assumes the first row contains heads.
+
+    the_data = getSheetData(sheet,range)
+    # use first row as heads
+    the_heads = the_data[0]
+
+    the_results = []
+
+    # generate combined query/target pairs to evaluate in rows.
+    # Will look like [ [ query_str1, [col_a,col_b] ], [ query_str2, [col_c,col_d] ] ]
+    the_query_pairs = []
+    for q in queries:
+    # Get list of column indexes for which the head matches query (should usually be just one or none).
+        the_col_indexes = [ind for ind, txt in enumerate(the_heads) if txt == q[0]]
+        the_query_pairs.append([q[1], the_col_indexes])
+
+    # Process each row testing against all query pairs; result is a list of booleans.
+    for row_num, row_data in enumerate(the_data):
+
+        # Adjust list to match row numbers in sheet, starting with 1
+        row_num = row_num + 1
+
+        res = []
+        for p in the_query_pairs:
+            the_col_data = [row_data[c] for c in p[1]]
+            if regex==True:
+                the_pattern = re.compile(p[0])
+                res_list = list(filter(the_pattern.search, the_col_data))
+            else:
+                res_list = the_col_data.count(p[0])
+                # TODO: this works but is clunky, diff type of object. 
+
+            if res_list: # if there is any matches...
+                res.append(True)
+            else:
+                res.append(False)
+
+        # Determine if row matches, depending on and/or junction
+        is_hit = False
+        if operator == 'and': # and: all must be True
+            if not(False in res):
+                is_hit = True
+
+        else: # default 'or' junction: at least one must be True
+            if True in res:
+                is_hit = True
+
+        if is_hit == True:
+
+            # Check if the row is alredy in the results, and add if not.
+            if not( [r["row"] for r in the_results if r["row"] == row_num]):
+                the_row_info = {}
+                the_row_info["row"] = row_num
+                the_row_info["data"] = row_data
+                # add the row to the results.
+                the_results.append(the_row_info)
+
+    if len(the_results) > 0:
+        # Add heads as first row.
+        the_results.insert(0, {'row':1, 'data': the_heads})
+        # sort the results by row number.
+        the_results = sorted(the_results, key=lambda k: k['row'])
+
+    # Result (if any) will be list of dicts, with head row as first item for further processing if needed.
+    # Each dict is of form {'row': <integer>, 'data': [<col1_data>, <col2_data>, etc.]}
+    return the_results
+
 
 
 
--- sample.py
+++ sample.py
@@ -1,16 +1,33 @@
 import GoogleSheetAPITools as gs
 import secrets
+from pprint import pprint
+
 
 # The ID and range of a sample spreadsheet.
 test_sheet = secrets.default_sheet
-test_range = 'Test!A:Z'
+test_range = secrets.default_range
 
 
 def main():
 
 #    print(gs.sheetLookup(test_sheet,test_range,"b",1,0))
 
-    c = gs.sheetClear(test_sheet,test_range)
+    # Clear contents of sheet
+    gs.sheetClear(test_sheet,test_range)
+
+    # Query rows that match two regex queries (AND junction)
+    x = gs.getMatchingRows(the_sheet, the_range, [['linked_records_record_id', 'resource'],['linked_records_record_title', 'Marvin']], regex=True, operator='and')
+
+    pprint(x)
+
+    # Write data to sheet
+    gs.sheetAppend(the_sheet,the_target,the_data)
+
+    # Get list tab names from sheet
+    the_tabs = gs.getSheetTabs(test_sheet)
+
+    # Get data from 2nd tab
+    the_data = gs.getSheetData(test_sheet, str(the_tabs[1] + '!A:Z'))
 
 
 
